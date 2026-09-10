64356d457d27f1d8b67c1364145f14230fd5ac57
Nguyễn Chí Nam <ncn522005@gmail.com>
2026-09-10 12:10:04 +0700

    Complete Lab 03 Docker MERN

--- project/client/vite.config.js
+++ project/client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'http://host.docker.internal:5000'
     }
   }
 })
--- project/server/server.js
+++ project/server/server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
+
 const mongoose = require("mongoose");
+
 const cors = require("cors");
 
 const studentRoutes = require("./routes/studentRoutes");
@@ -9,18 +11,32 @@ const studentRoutes = require("./routes/studentRoutes");
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
+// Câu 45: API kiểm tra Backend
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from Express Backend!" });
+});
 
 app.use("/api/students", studentRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
+
     .then(() => {
+
         console.log("MongoDB connected successfully!");
+
     })
+
     .catch((error) => {
+
         console.error("MongoDB connection error:", error);
+
     });
 
 app.listen(5000, "0.0.0.0", () => {
+
     console.log("Server running on port 5000");
+
 });
